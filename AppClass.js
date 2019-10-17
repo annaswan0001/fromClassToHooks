a76d0a0f496ee6fd45c4255cb818148d2609b37f
A.Kyzmenko <a.kyzmenko@MacBook-Air-Anna.local>
2019-10-17 09:49:00 +0400

    transfer from class to hooks

---
+++ AppClass.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    count: 0,
+    isOn: false,
+    x: null,
+    y: null
+  };
+
+  componentDidMount() {
+    document.title = `You have been clicked ${this.state.count} times`;
+    window.addEventListener("mousemove", this.handleMouseMove);
+  }
+
+  componentDidUpdate() {
+    document.title = `You have been clicked ${this.state.count} times`;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+  }
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.pageX,
+      y: event.pageY
+    });
+  };
+
+  incrementCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {this.state.count} times
+        </button>
+
+        <h2>Toggle Light</h2>
+        <div
+          style={{
+            height: "50px",
+            width: "50px",
+            background: this.state.isOn ? "yellow" : "grey"
+          }}
+          onClick={this.toggleLight}
+        />
+
+        <h2>Mouse Position</h2>
+        <p>X position: {this.state.x}</p>
+        <p>Y position: {this.state.y}</p>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ AppFunction.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  );
+  let mounted = true;
+
+  useEffect(
+    () => {
+      document.title = `You have clicked ${count} times`;
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("online", handleOnline);
+      window.addEventListener("offline", handleOffline);
+      navigator.geolocation.getCurrentPosition(handleGeolocation);
+      const watchId = navigator.geolocation.watchPosition(handleGeolocation);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("online", handleOnline);
+        window.removeEventListener("offline", handleOffline);
+        navigator.geolocation.clearWatch(watchId);
+        mounted = false;
+      };
+    },
+    [count]
+  );
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
+
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    });
+  };
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+  };
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/aaa"
+        }
+        style={{
+          height: "50px",
+          width: "50px"
+        }}
+        alt="Flashlight"
+        onClick={toggleLight}
+      />
+
+      <h2>Mouse Position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
+
+      <h2>Network Status</h2>
+      <p>
+        You are <strong>{status ? "online" : "offline"}</strong>
+      </p>
+
+      <h2>Geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is {speed ? speed : "0"}</p>
+    </>
+  );
+};
+
+export default App;
